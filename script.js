87c1a65d40384dc0eda7c409e80be35a1bc9c4ed
IGOR <ghikltv@gmail.com>
2024-10-01 20:40:10 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById("scrollToTop").addEventListener("click", function() {
+    window.scrollTo({ top: 20, behavior: 'smooth' });
+});
+
+document.getElementById("scrollToMid").addEventListener("click", function() {
+    window.scrollTo({ top: 400, behavior: 'smooth' });
+});
+
